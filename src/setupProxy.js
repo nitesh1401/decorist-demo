1d9d2ed3d28c6302497be389203949efc8cd9ed2
Nikhil Verma <nikhil.verma@decorist.com>
2018-11-29 18:11:41 +0530

    changed to dev.decorist

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,7 +1,7 @@
 const proxy = require('http-proxy-middleware');
     module.exports = function(app) {
     app.use(proxy('/api', 
-          { target: 'http://users.localhost' ,
+          { target: 'http://users.dev.decorist.com' ,
             changeOrigin: true,
             logLevel: "debug",
           }
