1004d514c24fbfdc2a7b8f43aa02bd323803782e
Nikhil Verma <nikhil.verma@decorist.com>
2018-11-29 16:24:46 +0530

    setupProxy file is working fine

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,6 +1,9 @@
 const proxy = require('http-proxy-middleware');
     module.exports = function(app) {
     app.use(proxy('/api', 
-                   { target: 'http://users.dev.decorist.me' }
+          { target: 'http://users.localhost' ,
+            changeOrigin: true,
+            logLevel: "debug",
+          }
       ));
     }
